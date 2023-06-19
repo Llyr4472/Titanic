0b62b50b530513ccdea6e090098d37d776e6474b
Llŷr <mr.prashant2077@gmail.com>
2023-06-19 19:32:15 +0545

    update titanic.py

--- titanic.py
+++ titanic.py
@@ -1,34 +1,92 @@
 # %%
 import numpy as np
-import os
-import sys
+import pandas as pd
 import tensorflow as tf
 
+from sklearn.model_selection import train_test_split
+
+# %%
+EPOCHS = 20
+TEST_SIZE = 0.4
+DROPOUT = 0.5
 
 # %%
 def main():
 
     #load data
-    test_f = "test.csv"
     train_f = "train.csv"
-    X_train, Y_train = load_data(train_f)
-    X_test = load_data(test_f)
+    evidence, label = load_data(train_f)
+    x_train, x_test, y_train, y_test =  train_test_split(evidence,label,test_size=TEST_SIZE)
 
-    #Train model and predict results
-    model = train_model(X_train, Y_train)
-    predictions = model.predict(X_test)
-    sensitivity, specificity = evaluate(Y_test, predictions)
+    # Get a compiled neural network
+    model = get_model()
 
-    # Print results
-    print(f"Correct: {(y_test == predictions).sum()}")
-    print(f"Incorrect: {(y_test != predictions).sum()}")
-    print(f"True Positive Rate: {100 * sensitivity:.2f}%")
-    print(f"True Negative Rate: {100 * specificity:.2f}%")
+    # Fit model on training data
+    model.fit(x_train, y_train, epochs=EPOCHS)
+
+    # Evaluate neural network performance
+    model.evaluate(x_test,  y_test, verbose=2)
 
 # %%
-def load_data(file):
-    #TODO
+def load_data(filename):
+
+    #load data
+    data = pd.read_csv(filename)
+
+    #check for missing data
+    data.isnull().sum()
+
+    # Fill missing values in the "Age" column with the median
+    data["Age"].fillna(0, inplace=True)
+
+    # One-hot encode the columns
+    data = pd.get_dummies(data, columns=["Sex", "Embarked"])
+
+    # Convert boolean columns to integers
+    boolean_columns = ["Sex_female", "Sex_male", "Embarked_C", "Embarked_Q", "Embarked_S"]
+    data[boolean_columns] = data[boolean_columns].astype(int)
+
+
+    # Perform min-max scaling on the "Age" and "Fare" columns
+    data["Age"] = (data["Age"] - data["Age"].min()) / (data["Age"].max() - data["Age"].min())
+    data["Fare"] = (data["Fare"] - data["Fare"].min()) / (data["Fare"].max() - data["Fare"].min())
+
+    # Select features and target
+    features = ["Pclass", "Age", "SibSp", "Parch", "Fare", "Sex_female", "Sex_male", "Embarked_C", "Embarked_Q", "Embarked_S"]
+    target = "Survived"
+
+    data.to_csv(filename.split(".")[0]+"panda.csv", index=True)
+
+    x = data[features]
+    y = data[target]
+    return x, y
+
+
 
 # %%
-def train_model(x,y):
-    #TODO
+def get_model():
+    model =tf.keras.Sequential([
+
+        #hidden layer
+        tf.keras.layers.Dense(256,input_shape=(10,), activation="relu"),
+        tf.keras.layers.Dense(128,input_shape=(10,), activation="relu"),
+        tf.keras.layers.Dense(64,input_shape=(10,), activation="relu"),
+
+        #dropout layer
+        tf.keras.layers.Dropout(DROPOUT),
+
+        #output layer
+        tf.keras.layers.Dense(1,activation="sigmoid"),
+    ])
+
+    #compile and return
+    model.compile(
+        optimizer="adam",
+        loss = "binary_crossentropy",
+        metrics = [tf.keras.metrics.Precision(),"accuracy"],
+    )   
+    return model
+
+# %%
+if __name__ == "__main__":
+    main()
